4e6c83067d0176a020dba9293043a4ce9098c8ba
Occultgoyo <leiria0109@gmail.com>
2022-09-29 15:18:43 +0900

    아마도 마지막

--- src/Home.js
+++ src/Home.js
@@ -5,15 +5,31 @@ function Home() {
   return (
     <div className="home">
       <div className="home_container">
-        {/* <img className="home_backimg" src="" alt="" /> */}
-
         <div className="home_list">
-          <Product id="1" title = "Razer Blade" price={6785850} image={ process.env.PUBLIC_URL + "/images/Razer_blade.png" }/>
-          <Product id="2" title = "End Custom PC " price={10551300} image={ process.env.PUBLIC_URL +  "/images/CustomPC.png"}/>
-        {/* </div>
-        <div className="home_list"> */}
-        <Product id="3" title = "Predator Throne" price={20015800} image={process.env.PUBLIC_URL + "/images/Predator_Throne.png"}/>
-        <Product id="4" title = "Zoom Lilac" price={670000} image={ process.env.PUBLIC_URL +  "/images/zoom_lilac.png" }/>
+          <Product
+            id="1"
+            title="Razer Blade"
+            price={6785850}
+            image={process.env.PUBLIC_URL + "/images/Razer_blade.png"}
+          />
+          <Product
+            id="2"
+            title="End Custom PC "
+            price={10551300}
+            image={process.env.PUBLIC_URL + "/images/CustomPC.png"}
+          />
+          <Product
+            id="3"
+            title="Predator Throne"
+            price={20015800}
+            image={process.env.PUBLIC_URL + "/images/Predator_Throne.png"}
+          />
+          <Product
+            id="4"
+            title="Zoom Lilac"
+            price={670000}
+            image={process.env.PUBLIC_URL + "/images/zoom_lilac.png"}
+          />
         </div>
       </div>
     </div>
